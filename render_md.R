b8b1d596e95e836d7a6f2f9d257aadb5801c1b1c
bphigg <bphigg@gmail.com>
2023-10-06 12:57:43 -0400

    md trial

---
+++ render_md.R
@@ -0,0 +1,10 @@
+rmarkdown::render("~/R/Project_2/git_temp.Rmd", 
+                  output_file = "~/R/Project_2/README.md", 
+                  output_format = "md_document",
+                  output_options = list(toc=TRUE, toc_depth=2,
+                                        number_sections=TRUE, df_print="default"))
+
+
+#rmarkdown::render(  input = 'file.RMD' , output_file = 'file.md'
+ #                   , output_format=   md_document( ) )  
+
