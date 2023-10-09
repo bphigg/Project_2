1776cb781ec46c53a409c5e17d4ba32975d4f3ed
bphigg <bphigg@gmail.com>
2023-10-09 15:53:46 -0400

    2nd commit

---
+++ player_stats.R
@@ -0,0 +1,22 @@
+player_id <- function(last_name, first_name = ""){
+  #last_name = noquote(last_name)
+  #first_name = noquote(first_name)
+  url <- "https://free-nba.p.rapidapi.com/players"
+  
+  queryString <- list(
+    page = "0",
+    per_page = "25",
+    search = paste(first_name, last_name)
+  )
+  
+  response <- VERB("GET", url, query = queryString, add_headers('X-RapidAPI-Key' = '3009c8c91amsh65cad163db7085ap15d3d2jsnad2333f55e76', 'X-RapidAPI-Host' = 'free-nba.p.rapidapi.com'), content_type("application/octet-stream"))
+  
+  id <- fromJSON(rawToChar(response$content))$data
+  id <- bind_cols(id %>% select(-team), id$team)
+  attributes(id)$names[1] <- "player_id"
+  attributes(id)$names[8] <- "team_id"
+  attributes(id)$names[9] <- "team"
+  
+  return(id %>% select("player_id", "first_name", "last_name", "team"))
+}
+temp <- player_id("finley", "")
---
+++ stats_api.R
@@ -0,0 +1,24 @@
+library(httr)
+
+url <- "https://free-nba.p.rapidapi.com/stats?seasons[]=2009&player_ids[]=942"
+
+queryString <- list(
+  page = "0",
+  per_page = "25"
+)
+
+response <- VERB("GET", url, query = queryString, add_headers('X-RapidAPI-Key' = '3009c8c91amsh65cad163db7085ap15d3d2jsnad2333f55e76', 'X-RapidAPI-Host' = 'free-nba.p.rapidapi.com'), content_type("application/octet-stream"))
+
+content(response, "text")
+
+library(dplyr)
+library(jsonlite)
+
+str(response)
+parsed <- fromJSON(rawToChar(response$content))
+str(parsed)
+head(parsed)
+parsed$data
+parsed <- parsed$data
+str(parsed)
+parsed
