15b86b0745177516ea19542c7409e71884c14366
bphigg <bphigg@gmail.com>
2023-10-10 09:01:59 -0400

    more stats updates

--- player_stats_year.R
+++ player_stats_year.R
@@ -14,15 +14,19 @@ player_stats_year <- function(last_name, first_name = "", year){
     response <- VERB("GET", url, add_headers('X-RapidAPI-Key' = '3009c8c91amsh65cad163db7085ap15d3d2jsnad2333f55e76', 'X-RapidAPI-Host' = 'free-nba.p.rapidapi.com'), content_type("application/octet-stream"))
     
     temp <- fromJSON(rawToChar(response$content))$data
+    temp <- bind_cols(temp %>% select(-team), temp$team)
+    temp <- bind_cols(temp %>% select(-game), temp$game)
+    temp <- bind_cols(temp %>% select(-player), temp$player)
     
-    temp <- temp %>% select("ast", "blk", "min", "pf", "pts", "reb", "stl")
+    temp <- temp %>% select("first_name", "last_name", "abbreviation", "date", "season", "home_team_id", "home_team_score", "visitor_team_id", "visitor_team_score", "ast", "blk", "min", "pf", "pts", "reb", "stl")
     stats <- rbind(stats, temp)
   }
   stats <- stats %>% mutate(min=substr(stats$min, 1, 2))
   stats$min <- as.numeric(stats$min)
+  attributes(stats)$names[3] <- "team"
   
-  #return(stats)
-  return(colMeans(select(stats, ast:stl), na.rm=TRUE))
+  return(stats)
+  #return(colMeans(select(stats, ast:stl), na.rm=TRUE))
 }
 player_stats_year("duncan", "tim", 2014)
 
