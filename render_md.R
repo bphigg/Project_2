1ab5d537b97fdd2797c90abe2de3b6b118c07c53
bphigg <bphigg@gmail.com>
2023-10-11 16:55:53 -0400

    render 1

--- render_md.R
+++ render_md.R
@@ -1,7 +1,7 @@
 rmarkdown::render("~/R/Project_2/project_2.Rmd", 
                   output_file = "~/R/Project_2/README.md", 
                   output_format = "md_document",
-                  output_options = list(toc=TRUE, toc_depth=2,
+                  output_options = list(toc=TRUE, toc_depth=1,
                                         number_sections=TRUE, df_print="default"))
 
 
